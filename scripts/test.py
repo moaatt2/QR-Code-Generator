088858fd57c9d2467dabd929eb0ce86f0d9f6318
Dakota McKay <dakotamckay72@gmail.com>
2025-03-29 08:24:50 -0400

    Allow user to specify what data goes in the qr code.

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,7 @@
 import qrcode
 
-img = qrcode.make('Some data here')
+data = input("What data do you want put in your QR Code?\n")
+
+img = qrcode.make(data)
 type(img)  # qrcode.image.pil.PilImage
 img.save("output/test.png")
