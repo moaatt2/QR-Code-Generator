14fccdf6d3e78d7662accaad16cedc6cf07e0aa5
Dakota McKay <dakotamckay72@gmail.com>
2025-03-29 08:49:42 -0400

    Add confirmation loop to qr code data.

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,16 @@
 import qrcode
 
-data = input("What data do you want put in your QR Code?\n")
 
+# Get and confirm data
+while True:
+    data = input("What data do you want put in your QR Code?\n")
+    print()
+    print(f"Are you sure that '{data}' is the what should be in the QR Code?")
+    choice = input("(y/n): ")
+    if choice.lower()[0] == "y":
+        break
+
+
+filename = input("What do you want to name the file?\n")
 img = qrcode.make(data)
-img.save("output/test.png")
+img.save(f"output/{filename}.png")
