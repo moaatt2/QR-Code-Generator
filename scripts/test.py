1c6d473b535e675e2fbed789bbf59e0bde973a49
Dakota McKay <dakotamckay72@gmail.com>
2025-03-29 08:53:27 -0400

    Clean interface by clearing terminal.

--- scripts/test.py
+++ scripts/test.py
@@ -1,20 +1,28 @@
+import os
 import qrcode
+
+
+# terminal clearing utilty function
+def clearTerminal():
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 # Get and confirm data
 while True:
     data = input("What data do you want put in your QR Code?\n")
-    print()
+    clearTerminal()
     print(f"Are you sure that '{data}' is the what should be in the QR Code?")
     choice = input("(y/n): ")
     if choice.lower()[0] == "y":
         break
 
+# Clear terminal after confirming data
+clearTerminal()
 
-# Get and confirm data
+# Get and confirm filename
 while True:
     filename = input("What do you want to name the file?\n")
-    print()
+    clearTerminal()
     print(f"Are you sure that '{filename}.png' is the filename you want?")
     choice = input("(y/n): ")
     if choice.lower()[0] == "y":
