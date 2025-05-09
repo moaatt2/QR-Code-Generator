e4fae26dc5c9e9ecca45f6e7423cc39327eb66fb
Dakota McKay <dakotamckay72@gmail.com>
2025-05-09 18:09:27 -0400

    Add rough layout of type section using notebooks to emulate windows settings tabslike behaviour.

--- scripts/gui_test.py
+++ scripts/gui_test.py
@@ -4,10 +4,24 @@ import qrcode
 import datetime as dt
 import validators
 import tkinter
+from tkinter import ttk
 
+# Create 
 window = tkinter.Tk()
-
 window.title("QR Code Generator")
+window.geometry("400x400")
 
-label = tkinter.Label(window, text="Enter the URL or text to encode:").pack()
+notebook = ttk.Notebook(window)
+
+raw_data = ttk.Frame(notebook)
+vCard = ttk.Frame(notebook)
+
+notebook.add(raw_data, text="Raw Data")
+notebook.add(vCard, text="vCard")
+
+notebook.pack(expand=True, fill="both")
+
+code_image = tkinter.PhotoImage(file="output/test.png")
+update_button = tkinter.Button(window, text="Update QR Code").pack()
+
 window.mainloop()
