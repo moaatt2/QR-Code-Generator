ee4dc4df820cda29853f25f87bd8e1b60c11b03d
Dakota McKay <dakotamckay72@gmail.com>
2025-05-10 12:32:45 -0400

    Enhance GUI layout by adding a sidebar and organizing notebook tabs for QR code generation

--- scripts/gui_test.py
+++ scripts/gui_test.py
@@ -6,22 +6,34 @@ import validators
 import tkinter
 from tkinter import ttk
 
-# Create 
+# Create primary frame to hold QR sidebar and content entry notebook
 window = tkinter.Tk()
 window.title("QR Code Generator")
 window.geometry("400x400")
 
-notebook = ttk.Notebook(window)
+# Create Main Frame to hold notebook and sidebar
+main_frame = tkinter.Frame(window)
+main_frame.pack(expand=True, fill="both")
 
+# Create sidebar
+sidebar = tkinter.Frame(main_frame, width=200)
+sidebar.pack(side="right", fill="y")
+ttk.Button(sidebar, text="Update QR Code").pack(side="bottom", fill="x")
+
+# Create notbook tabs
+notebook_frame = tkinter.Frame(main_frame)
+notebook_frame.pack(side="left", expand=True, fill="both")
+notebook = ttk.Notebook(notebook_frame)
+
+# Create Notebook Tabs
 raw_data = ttk.Frame(notebook)
 vCard = ttk.Frame(notebook)
 
+# Add Tabs to Notebook
 notebook.add(raw_data, text="Raw Data")
 notebook.add(vCard, text="vCard")
 
+# Add notebook to UI
 notebook.pack(expand=True, fill="both")
-
-code_image = tkinter.PhotoImage(file="output/test.png")
-update_button = tkinter.Button(window, text="Update QR Code").pack()
 
 window.mainloop()
