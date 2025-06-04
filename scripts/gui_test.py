a2a6dbe8bbe2ce8ba95cc8262413cd30452c2110
Dakota McKay <dakotamckay72@gmail.com>
2025-06-04 17:57:22 -0400

    Simplify function at cost of generatlity.

--- scripts/gui_test.py
+++ scripts/gui_test.py
@@ -270,14 +270,14 @@ notebook.pack(expand=True, fill="both")
 
 ## Combobox Behaviour
 
-def block_and_forward_scroll(event, widget):
-    widget.event_generate("<MouseWheel>", delta=event.delta)
+def block_and_forward_scroll(event):
+    vCard.event_generate("<MouseWheel>", delta=event.delta)
     return "break"
 
 for box in [phone_type, email_type]:
 
     # Ignore highlighting value after selection
     box.bind("<<ComboboxSelected>>", lambda e: e.widget.selection_clear())
-    box.bind("<MouseWheel>", lambda e, w=vCard: block_and_forward_scroll(e, w))
+    box.bind("<MouseWheel>", lambda e: block_and_forward_scroll(e))
 
 window.mainloop()
