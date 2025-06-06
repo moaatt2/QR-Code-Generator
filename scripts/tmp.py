3625889dbe3e31c3e0456807c60e36013af845bd
Dakota McKay <dakotamckay72@gmail.com>
2025-06-06 17:24:52 -0400

    Reduce example further

--- scripts/tmp.py
+++ scripts/tmp.py
@@ -21,30 +21,8 @@ email_types = [
 window = tkinter.Tk()
 window.title("QR Code Generator")
 window.geometry("400x400")
-
-# Create Main Frame to hold notebook and sidebar
-main_frame = tkinter.Frame(window)
-main_frame.pack(expand=True, fill="both")
-
-# Create sidebar
-sidebar = tkinter.Frame(main_frame, width=200)
-sidebar.pack(side="right", fill="y")
-ttk.Button(sidebar, text="Update QR Code").pack(side="bottom", fill="x")
-
-# Create notebook
-notebook_frame = tkinter.Frame(main_frame)
-notebook_frame.pack(side="left", expand=True, fill="both")
-notebook = ttk.Notebook(notebook_frame)
-
-# Create Tabs to add to notebook
-raw_data = ttk.Frame(notebook)
-vCard = ScrolledFrame(notebook, autohide=False)
-
-# Add Content to Raw Data Tab
-label = tkinter.Label(raw_data, text="QR Code Data:")
-label.config(font=("Arial", 10, "bold"))
-label.pack(pady=5)
-tkinter.Text(raw_data).pack(padx=10, pady=5, fill="both", expand=True)
+vCard = ScrolledFrame(window, autohide=False)
+vCard.pack(fill="both", expand=True)
 
 ######################################
 ### Add Email Section to vCard Tab ###
@@ -75,7 +53,6 @@ email_type.grid(row=28, column=1, sticky="ew", padx=(0, 15))
 ttk.Separator(vCard, orient="horizontal").grid(row=29, columnspan=2, sticky="ew", pady=5)
 
 
-
 ######################################
 ### Add Filler To Ensure Scrolling ###
 ######################################
@@ -83,18 +60,6 @@ ttk.Separator(vCard, orient="horizontal").grid(row=29, columnspan=2, sticky="ew"
 for i in range(30, 50):
     tkinter.Label(vCard, text="Suffix:").grid(row=i, sticky="e")
     tkinter.Entry(vCard).grid(row=i, column=1, sticky="ew", padx=(0, 15))
-
-
-#################################
-#### Add Sections to Notebook ###
-#################################
-
-# Add Tabs to Notebook
-notebook.add(raw_data, text="Raw Data")
-notebook.add(vCard.container, text="vCard")
-
-# Add notebook to UI
-notebook.pack(expand=True, fill="both")
 
 
 #####################
